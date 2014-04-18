73207dcb0269383fcc7bc773989f5fe65871c495
Raphael Perraudeau <r.perraudeau@gmail.com>
2014-04-18 11:25:15 +0200

    better visibility on facebook sharing

--- header.php
+++ header.php
@@ -15,6 +15,9 @@ namespace ThemeCheck;
 		<meta name="viewport" content="width=device-width">
 
 		<meta property="og:image" content="<?php echo TC_HTTPDOMAIN;?>/img/logo.jpg" />
+		<meta property="og:url" content="<?php echo TC_HTTPDOMAIN;?>" />
+		<meta property="og:title" content="<?php echo $controller->meta["title"]; ?>" />
+		<meta property="og:description" content="<?php echo $controller->meta["description"]; ?>" />
 		
 		<link rel="stylesheet" href="<?php echo TC_HTTPDOMAIN;?>/css/bootstrap.min.css">
 		<style>
