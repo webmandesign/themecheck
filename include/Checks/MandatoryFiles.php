8c9a379bc5438abee3e33cc96315d49bb1ab11e7
Guillaume BAUDHUIN <g.baudhuin@peoleo.fr>
2014-05-05 16:35:09 +0200

    Add check for presence of comments.php in wordpress themes

--- include/Checks/MandatoryFiles.php
+++ include/Checks/MandatoryFiles.php
@@ -30,6 +30,7 @@ class MandatoryFiles extends Check
 			$this->checks = array(
 						new MandatoryFiles_Checker('MANDATORYFILES_INDEXPHP', TT_JOOMLA | TT_WORDPRESS, ERRORLEVEL_CRITICAL, __all('Presence of file index.php'), 'index.php', 'ut_mandatoryfiles_index.zip'),
 						new MandatoryFiles_Checker('MANDATORYFILES_STYLECSS', TT_WORDPRESS | TT_WORDPRESS_CHILD, ERRORLEVEL_WARNING, __all('Presence of file style.css'), 'style.css', 'ut_mandatoryfiles_style.zip'),
+						new MandatoryFiles_Checker('MANDATORYFILES_COMMENTSPHP', TT_WORDPRESS, ERRORLEVEL_INFO, __all('Presence of file comments.php'), 'comments.php', 'ut_mandatoryfiles_todo.zip'),
 						new MandatoryFiles_Checker('MANDATORYFILES_TEMPLATEDETAILSXML', TT_JOOMLA, ERRORLEVEL_CRITICAL, __all('Presence of file templateDetails.xml'), 'templateDetails.xml', 'ut_mandatoryfiles_templatedetails.zip'),
 						new MandatoryFiles_Checker('MANDATORYFILES_TEMPLATE_THUMBNAILPNG', TT_JOOMLA, ERRORLEVEL_CRITICAL, __all('Presence of file template_thumbnail.png'), 'template_thumbnail.png', 'ut_mandatoryfiles_template_thumbnail.zip')
 			);
