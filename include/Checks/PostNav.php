d0470e014c7c09ab2744fc2af067f2c0794df653
Guillaume <g.baudhuin@peoleo.fr>
2016-02-25 18:25:43 +0100

    Add support for the_posts_navigation in postnav check

--- include/Checks/PostNav.php
+++ include/Checks/PostNav.php
@@ -9,10 +9,10 @@ class PostNav_Checker extends CheckPart
         $this->errorLevel = ERRORLEVEL_SUCCESS;
         $php = implode( ' ', $php_files );
 				
-        if ( strpos( $php, 'posts_nav_link' ) === false && strpos( $php, 'paginate_links' ) === false && strpos( $php, 'the_posts_pagination' ) === false &&
+        if ( strpos( $php, 'posts_nav_link' ) === false && strpos( $php, 'paginate_links' ) === false && strpos( $php, 'the_posts_pagination' ) === false && strpos( $php, 'the_posts_navigation' ) === false &&
               strpos( $php, 'previous_posts_link' ) === false && strpos( $php, 'next_posts_link' ) === false 
            ) {
-                $this->messages[] = __all('The theme doesn&#39;t have post pagination code in it. Use <strong>posts_nav_link()</strong> or <strong>paginate_links()</strong> or <strong>the_posts_pagination</strong> or <strong>next_posts_link()</strong> and <strong>previous_posts_link()</strong> to add post pagination.');
+                $this->messages[] = __all('The theme doesn&#39;t have post pagination code in it. Use <strong>posts_nav_link()</strong> or <strong>paginate_links()</strong> or <strong>the_posts_pagination()</strong> or <strong>the_posts_navigation()</strong> or <strong>next_posts_link()</strong> and <strong>previous_posts_link()</strong> to add post pagination.');
                 $this->errorLevel = $this->threatLevel;
         }
     }
